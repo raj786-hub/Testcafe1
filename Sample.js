eebcda47688cd93a5e0c12e3a9b6ed77054b6b2f
raj786-hub <76934322+raj786-hub@users.noreply.github.com>
2021-01-15 13:22:59 +0530

    Update Sample.js

--- Sample.js
+++ Sample.js
@@ -6,14 +6,5 @@ fixture `New Fixture`
 test('New Test', async t => {
     await t
         .click(Selector('header a').withText('LOGIN').nth(1))
-        .typeText('#userName', 'rajkumar2')
-        .typeText('#userPassword', '123@Mummy')
-        .click(Selector('button').withText('SIGN IN'))
-        .click(Selector('header a').withText('News & Events'))
-        .click(Selector('#mainContent a').withText('Conference Presentations'))
-        .click(Selector('#mainContent a').withText('Press Release News'))
-        .click(Selector('#mainContent a').withText('Webinars'))
-        .click(Selector('#mainContent a').withText('Newsletter'))
-        .click(Selector('header a').withText('LOGOUT').nth(1));
-    console.log("test completed")
+ console.log("test completed")
     });
